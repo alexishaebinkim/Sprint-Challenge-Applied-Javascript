0e940d900edf37dcdef07eaed782757e07bba05e
Haebin Kim <haebinkim@haebins-mbp.lan>
2019-05-26 16:52:56 -0700

    TabLink constructor

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,23 +14,19 @@ class TabLink {
       this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.card[data-tab="${this.data}"]`);
+      this.cards = document.querySelector(`.card[data-tab="${this.tabData}"]`);
     }
 
-    // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. 
+    // Map over the newly converted NodeList we just created in our if statement above. 
+    //Convert each this.cards element into a new instance of the TabCard class. 
     //Pass in a card object to the TabCard class. 
     
     Array.from(this.cards).map(card => {
-      console.log(card);
       return new TabCard(card);
-
     });
-    
 
     // Add a click event that invokes this.selectTab
-    this.tabElement.addEventListener('click', () => {
-      this.selectTab();
-    });
+    this.tabElement.addEventListener('click', () => this.selectTab());
   }
 
   selectTab(){
